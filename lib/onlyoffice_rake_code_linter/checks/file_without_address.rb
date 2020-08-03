857e2aaedf668e2e80ce6b7125cc22fdc3d92762
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-03 14:57:08 +0300

    Extract to class

--- lib/onlyoffice_rake_code_linter/checks/file_without_address.rb
+++ lib/onlyoffice_rake_code_linter/checks/file_without_address.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-# Task with check latvian address
 module OnlyofficeRakeCodeLinter
-  def file_without_address
-    latvian_address = 'LV-1050'
-    all_js_files = Dir['./**/*.js']
-    files_without_address = []
-    all_js_files.each do |file|
-      next if excluded_source_paths.any? { |exclude| file.include?(exclude) }
+  # Task with check latvian address
+  class OnlyofficeRakeCodeLinter
+    def self.file_without_address
+      latvian_address = 'LV-1050'
+      all_js_files = Dir['./**/*.js']
+      files_without_address = []
+      all_js_files.each do |file|
+        next if excluded_source_paths.any? { |exclude| file.include?(exclude) }
 
-      files_without_address << file unless File.read(file).include?(latvian_address)
+        files_without_address << file unless File.read(file).include?(latvian_address)
+      end
+
+      return if files_without_address.empty?
+
+      raise("Files without latvian address `#{latvian_address}`: #{files_without_address}")
     end
-
-    return if files_without_address.empty?
-
-    raise("Files without latvian address `#{latvian_address}`: #{files_without_address}")
   end
 end
